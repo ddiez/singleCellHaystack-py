76492b7a2dcfc1d64a7454163ad6caf7e079df1b
Diego Diez <diego10ruiz@gmail.com>
2022-08-25 18:37:51 +0900

    Fix toy data location.

--- singleCellHaystack/_data.py
+++ singleCellHaystack/_data.py
@@ -2,5 +2,5 @@ import pkg_resources
 
 def load_toy():
     import scanpy as sc
-    stream = pkg_resources.resource_stream(__name__, 'data/toy.h5ad')
+    stream = pkg_resources.resource_stream(__name__, 'datasets/toy.h5ad')
     return sc.read_h5ad(stream)
