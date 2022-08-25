1db5c8ca67ad565dbe668bbd6643096f43beb779
Diego Diez <diego10ruiz@gmail.com>
2022-08-25 21:07:01 +0900

    Update tests.

--- tests/test_sp.py
+++ tests/test_sp.py
@@ -6,7 +6,20 @@ adata_raw=adata.raw.to_adata()
 
 coord=adata_raw.obsm["X_umap"]
 exprs=adata_raw.X
+genes=adata_raw.var_names
 
-foo=hs.haystack_sparse(exprs, coord)
+foo=hs.haystack_array(exprs, coord, features=genes)
+print(foo["results"])
 
-print(foo)
+foo2=hs.haystack_adata(adata_raw, "X_umap")
+print(foo2["results"])
+
+foo3=hs.haystack(adata_raw, "X_umap")
+print(foo3["results"])
+
+foo4=hs.haystack(exprs, coord, features=genes)
+print(foo4["results"])
+
+import numpy as np
+foo5=hs.haystack(np.asarray(exprs.todense()), coord, features=genes)
+print(foo5["results"])
