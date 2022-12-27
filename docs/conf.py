2f463457b0cdbff912560dd089ac137a4968b8d4
Diego Diez <diego10ruiz@gmail.com>
2022-12-27 19:33:43 +0900

    Update docs/conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,19 @@
-extensions = ["myst_parser"]
+extensions = [
+  "sphinx_rtd_theme",
+  "sphinx.ext.autodoc",
+  "sphinx.ext.autosummary",
+  "nbsphinx"
+]
+
+#import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+
+project="singleCellHaystack"
+
+html_context = dict(
+    display_github=True,      # Integrate GitHub
+    github_user='ddiez',   # Username
+    github_repo='singleCellHaystack-py',     # Repo name
+    github_version='main',  # Version
+    conf_py_path='/',    # Path in the checkout to the docs root
+)
