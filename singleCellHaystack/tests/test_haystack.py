0afcf6aba48cdc9eb2a06d195a45f3180afe5b92
Diego Diez <diego10ruiz@gmail.com>
2023-01-05 13:12:58 +0900

    Add more test cases.

--- singleCellHaystack/tests/test_haystack.py
+++ singleCellHaystack/tests/test_haystack.py
@@ -1,6 +1,9 @@
 import singleCellHaystack as hs
+import pandas as pd
 
 def test_toy():
   adata = hs.load_toy()
   res = hs.haystack(adata, "tsne")
-  assert res 
+  assert res
+  assert res["results"] is not None
+  assert isinstance(res["results"], pd.core.frame.DataFrame) is True
