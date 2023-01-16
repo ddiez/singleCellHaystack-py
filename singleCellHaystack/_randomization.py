8a7db88f5f842a4b0c7d8760035122b8451bc39c
Diego Diez <diego10ruiz@gmail.com>
2023-01-16 20:21:20 +0900

    Add random_state argument to randomize_KLD.

--- singleCellHaystack/_randomization.py
+++ singleCellHaystack/_randomization.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.sparse import isspmatrix
-from random import sample
+from random import sample, seed
 from tqdm import tqdm
 
 def select_genes_to_randomize(x, ngenes=100, method="heavytails", tail=10, verbose=False):
@@ -19,7 +19,7 @@ def select_genes_to_randomize(x, ngenes=100, method="heavytails", tail=10, verbo
     ms = index[np.linspace(tail, index.shape[0]-tail-1, ngenes, dtype="int")]
     return np.concatenate([ls, ms, rs])
 
-def randomize_KLD(density, expression, Q, n_randomizations=100, pseudo=1e-300, verbose=False):
+def randomize_KLD(density, expression, Q, n_randomizations=100, pseudo=1e-300, random_state=None, verbose=False):
   from ._kld import calculate_KLD
 
   if (verbose):
@@ -33,6 +33,9 @@ def randomize_KLD(density, expression, Q, n_randomizations=100, pseudo=1e-300, v
   ngenes = expression.shape[1]
 
   KLD_rand=np.zeros([ngenes, n_randomizations])
+
+  if random_state is not None:
+    seed(random_state)
 
   for n in range(n_randomizations):
     shuffled_cells = sample(range(ncells), ncells)
