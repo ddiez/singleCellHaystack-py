0e3d4f90c833203593129535f7d85bdad42b9287
Diego Diez <diego10ruiz@gmail.com>
2023-01-16 20:27:05 +0900

    Add tests to reproduce results with random_state.

--- singleCellHaystack/tests/test_haystack.py
+++ singleCellHaystack/tests/test_haystack.py
@@ -1,9 +1,13 @@
 import singleCellHaystack as hs
 import pandas as pd
+import numpy as np
 
 def test_toy():
   adata = hs.load_toy()
-  res = hs.haystack(adata, "tsne")
+  res = hs.haystack(adata, "tsne", random_state=1)
   assert res
   assert res["results"] is not None
   assert isinstance(res["results"], pd.core.frame.DataFrame) is True
+  sum = res["results"]
+  assert np.all(sum.gene.head(3) == ["gene_62", "gene_79", "gene_339"])
+  #assert np.all(sum.KLD.head(3) == [2.092214, 2.308899, 1.840823])
