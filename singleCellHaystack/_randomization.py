6feccd71b47e670bdea2e42a2921d5656e690b28
Diego Diez <diego10ruiz@gmail.com>
2023-06-07 19:34:15 +0900

    Fix shuffle for sparse matrices.

--- singleCellHaystack/_randomization.py
+++ singleCellHaystack/_randomization.py
@@ -50,7 +50,7 @@ def randomize_KLD2(density, expression, Q, n_randomizations=100, pseudo=1e-300, 
     print("> calculating randomized KLD ...")
     pbar = tqdm(total=n_randomizations)
 
-  if (isspmatrix(expression)):
+ # if (isspmatrix(expression)):
     expression = expression.tolil()
 
   ncells = expression.shape[0]
@@ -61,11 +61,17 @@ def randomize_KLD2(density, expression, Q, n_randomizations=100, pseudo=1e-300, 
   for n in range(n_randomizations): 
     # TODO: shuffle by column (features) independently.
     #expression = shuffle(expression, random_state=random_state)
-    for k in range(expression.shape[1]):
-      expression[:, k] = shuffle(expression[:, k], random_state=random_state)
-    
     if (isspmatrix(expression)):
+      expression = expression.tolil()
+      for k in range(expression.shape[1]):
+          expression[:, [k]] = shuffle(expression[:, [k]], random_state=random_state)
       expression = expression.tocsc()
+    else:
+      for k in range(expression.shape[1]):
+        expression[:, k] = shuffle(expression[:, k], random_state=random_state)
+    
+    #if (isspmatrix(expression)):
+    #  expression = expression.tocsc()
     P = calculate_P_matrix(density, expression)
     KLD_rand[:, n] = calculate_KLD2(P, Q)
     if (verbose):
