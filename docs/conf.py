828d4539d05029d29468d77011d851acdcc24633
Diego Diez <diego10ruiz@gmail.com>
2023-07-01 18:19:52 +0900

    Change to sphinx book theme.

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 extensions = [
-  "sphinx_rtd_theme",
+  "sphinx_book_theme",
   "sphinx.ext.autodoc",
   "sphinx.ext.autosummary",
   "nbsphinx",
@@ -7,9 +7,15 @@ extensions = [
 ]
 
 #import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 project="singleCellHaystack"
+
+html_theme_options = {
+  "path_to_docs": "docs/",
+  "use_repository_button": True,
+  "repository_url": "https://github.com/ddiez/singleCellHaystack-py"
+}
 
 html_context = dict(
     display_github=True,      # Integrate GitHub
