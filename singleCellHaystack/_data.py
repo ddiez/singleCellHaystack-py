1f836822044d8d9074831412dc0e6c4fd52e6574
Diego Diez <diego10ruiz@gmail.com>
2023-07-01 16:50:27 +0900

    Fix importlib_resources to importlib.resources.

--- singleCellHaystack/_data.py
+++ singleCellHaystack/_data.py
@@ -1,6 +1,6 @@
-import importlib_resources
+import importlib.resources
 
 def load_toy():
     import scanpy as sc
-    path = importlib_resources.files(__name__).joinpath("datasets/toy.h5ad")
+    path = importlib.resources.files(__name__).joinpath("datasets/toy.h5ad")
     return sc.read_h5ad(path)
